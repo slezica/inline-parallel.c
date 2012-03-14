f447215f80adb3252d58a3cd566f17b2bf96cfe3
Santiago Lezica <slezica89@gmail.com>
2012-03-14 08:58:16 -0300

    Made a few improvements

--- inline-parallel.c
+++ inline-parallel.c
@@ -1,45 +1,25 @@
-#include <unistd.h>
-#include <sys/syscall.h>
-#include <sys/types.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <pthread.h>
+#include <sys/wait.h>
 
-#define process for(; !fork(); exit(0))
-
-#define thread(name) pthread_t name;\
-    do {\
-    void* __inline_thread_func_ ## name(void* nothing) { do
-
-#define exit_thread(name) while(0); return NULL; }\
-    pthread_create(& name, NULL, & __inline_thread_func_ ## name, NULL);\
-    } while(0)
-
+#include "inpar.h"
 
 int main() {
-    int i;
+    pid_t child = proc {
+        printf("My pid is %d\n", getpid());
+    } endproc;
 
-    for (i = 0; i < 5; i++) {
-
-        process {
-            /* Five processes print this: */
-            printf("My pid is %d\n", getpid());
-        }
-
-    }
-
-    /* Only the original prints this: */
-    printf("And mine is %d\n", getpid());
-
-    /* Threads are slightly different: you provide a variable
-     * name to hold the thread structure: */
-    thread(my_thread) {
-        printf("My thread ID is: %d\n", (unsigned int) pthread_self());
-
-        /* And you have to end with this: */
-    } exit_thread(my_thread);
-
-    pthread_join(my_thread, NULL);
-
-    printf("And my thread ID is: %d\n", (unsigned int) pthread_self());
+    wait(NULL);
+    
+    printf("His pid was %d, mine is %d\n", child, getpid());
+    
+    pthread_t foo = thread {
+        printf("My thread ID is %u\n", (unsigned int) pthread_self());
+    } endthread;
+    
+    pthread_join(foo, NULL);
+    
+    printf("Joined %u. By the way, my ID is %u\n",
+           (unsigned int) foo,
+           (unsigned int) pthread_self()
+    );
 }
---
+++ inpar.h
@@ -0,0 +1,46 @@
+#ifndef _INPAR_H_
+#define _INPAR_H_
+
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+/* NOTE this header is GCC-only, as far as I know */
+
+/* Instead of escaping newlines in the macros below, I used the comment
+ * trick you'll see so that the output of gcc -E -CC is readable. */
+
+#define proc ({ /*
+*/  pid_t _inpar_pid_temp; /*
+*/  for (_inpar_pid_temp = fork(); !_inpar_pid_temp; exit(0))
+
+#define endproc _inpar_pid_temp; })
+/* Examples:
+ * 
+ * You can fire an anonimous process:
+ *   proc { 
+ *       (this code runs in another process)
+ *   } endproc;
+ * 
+ * Or, you can assign the child's pid to a variable:
+ *   pid_t pid = proc { 
+ *       (this code runs in another process)
+ * 
+ *   } endproc;
+ * 
+ * (pid holds the return of fork() for the parent)
+ * 
+ */
+ 
+#define thread ({ /*
+*/  pthread_t _inpar_thread_temp; /*
+*/  auto void* _inpar_thread_func(void* nothing); /*
+
+*/  pthread_create(&_inpar_thread_temp, NULL, &_inpar_thread_func, NULL);/*
+*/  void* _inpar_thread_func(void* nothing)
+
+#define endthread _inpar_thread_temp; })
+
+#endif
